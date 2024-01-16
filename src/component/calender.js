d96352098781c97898f8279451e091238fd9c45c
Naantuam <nathaniellongmen@gmail.com>
2024-01-16 12:07:47 +0100

    numberguesser

--- src/component/calender.js
+++ src/component/calender.js
@@ -4,7 +4,7 @@ export default function Calendar(){
     return(
         <div className="metro calendar" onclick="mail()">
             <div className="icon-chat"></div>
-            <span className="label bottom">calender</span>
+            <span className="label bottom"><a href="https://numberguesser-seven.vercel.app/"></a>Numberguesser</span>
         </div>
     )
 }
